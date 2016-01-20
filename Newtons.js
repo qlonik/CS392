0d469feda69967d9ee912bfecc938f46ae860293
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-01-20 00:54:25 -0400

    docs(Newtons): add jsdoc to function

--- Newtons.js
+++ Newtons.js
@@ -1,3 +1,19 @@
+/**
+ * Function which approximates the equation f(x) = 0 near the initial
+ * approximation and returns found approximation and the amount of steps
+ * required to find the approximation with specified tolerance.
+ *
+ * @param {Function} f - function for approximation
+ * @param {Function} fPrime - derivative of function f. Algorithm cannot find
+ * derivatives by itself, so we need to pass function which is a derivative of
+ * function f.
+ * @param {Number} appr - initial approximation
+ * @param {Number} tol - tolerance (precision) of the answer
+ * @param {Number} steps - maximum number of iterations
+ *
+ * @return {[Number, Number]|Error} Array with 2 numbers - solution x and the
+ * amount of steps.
+ */
 function Newtons(f, fPrime, appr, tol, steps) {
   let i = 1;
 
