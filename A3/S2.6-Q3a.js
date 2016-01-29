93957b374ba94e15f711389f829ca1d0f1466147
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-01-29 01:41:50 -0400

    style(A3_Q4): print string representation of complex number
    If we get complex answer, then print it as string instead of an object.

--- A3/S2.6-Q3a.js
+++ A3/S2.6-Q3a.js
@@ -10,6 +10,16 @@ const firstResult = Mullers(f, -3, -1.5, 0, pow(10, -4), 100);
 const secondResult = Mullers(f, 0, 1.5, 3, pow(10, -4), 100);
 const thirdResult = Mullers(f, 3, 4.5, 6, pow(10, -4), 100);
 
+if (typeof firstResult[0] === 'object') {
+  firstResult[0] = firstResult[0].toString();
+}
+if (typeof secondResult[0] === 'object') {
+  secondResult[0] = secondResult[0].toString();
+}
+if (typeof thirdResult[0] === 'object') {
+  thirdResult[0] = thirdResult[0].toString();
+}
+
 console.log(firstResult);
 console.log(secondResult);
 console.log(thirdResult);
