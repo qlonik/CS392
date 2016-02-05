957a960a44d72a05c0c36b23ecf1b8ae7bf578e7
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-02-05 01:54:56 -0400

    style(GaussianBackwardsSubst): remove unnecessary empty lines

--- GaussianBackwardsSubst.m
+++ GaussianBackwardsSubst.m
@@ -13,16 +13,13 @@ GaussianBackwardsSubst[AImmutable_] := Module[{A, n, i, x},
 
     While[p <= n && A[[p, i]] == 0, p++];
     If[p > n, Throw["no unique solution exists"]];
-
     If[p != i, A[[{p, i}]] = A[[{i, p}]]];
-
     For[j = i + 1, j <= n, j++, Module[{},
       A[[j]] -= A[[j, i]] / A[[i, i]] * A[[i]];
     ]];
   ]];
 
   If[A[[n, n]] == 0, Throw["no unique solution exists"]];
-
   x[[n]] = A[[n, n + 1]] / A[[n, n]];
   For[i = n - 1, i >= 1, i--, x[[i]] =
       (A[[i, n + 1]] - Sum[A[[i, j]] * x[[j]], {j, i + 1, n}]) / A[[i, i]]];
