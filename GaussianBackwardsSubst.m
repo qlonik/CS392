d0c7d811bb03080462009ab546d6a2146ea809aa
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-02-05 01:16:47 -0400

    feat(A4): Add implementation of Alg6.1 in Mathematica
    Algorithm 6.1 is "Gaussian Elimination with Backward Substitution."

---
+++ GaussianBackwardsSubst.m
@@ -0,0 +1,31 @@
+GaussianBackwardsSubst::usage =
+    "GaussianBackwardSubst[A] solves system of linear equations with indexes "
+        <> "specified in matrix A. This method assumes that the number of "
+        <> "unknowns is number of columns minus 1.";
+
+GaussianBackwardsSubst[AImmutable_] := Module[{A, n, i, x},
+  A = AImmutable;
+  n = Length[A[[1]]] - 1;
+  x = Table[0, n];
+
+  For[i = 1, i < n, i++, Module[{p, j},
+    p = i;
+
+    While[p <= n && A[[p, i]] == 0, p++];
+    If[p > n, Throw["no unique solution exists"]];
+
+    If[p != i, A[[{p, i}]] = A[[{i, p}]]];
+
+    For[j = i + 1, j <= n, j++, Module[{},
+      A[[j]] -= A[[j, i]] / A[[i, i]] * A[[i]];
+    ]];
+  ]];
+
+  If[A[[n, n]] == 0, Throw["no unique solution exists"]];
+
+  x[[n]] = A[[n, n + 1]] / A[[n, n]];
+  For[i = n - 1, i >= 1, i--, x[[i]] =
+      (A[[i, n + 1]] - Sum[A[[i, j]] * x[[j]], {j, i + 1, n}]) / A[[i, i]]];
+
+  Return[x];
+];
