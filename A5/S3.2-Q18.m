eecca49bd344b74ecabe933a4bdc8dbf6e97dc38
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-02-21 16:03:23 -0400

    feat(A5 Q4): correctly display output

--- A5/S3.2-Q18.m
+++ A5/S3.2-Q18.m
@@ -4,6 +4,6 @@ Import[FileNameJoin[{
 x = {1940, 1950, 1960, 1970, 1980, 1990};
 f = {132165000, 151326000, 179323000, 203302000, 226542000, 249633000};
 
-Print["1930: ", N[NewtonsDividedDifference[x, f][1930], 10]];
-Print["1965: ", N[NewtonsDividedDifference[x, f][1965], 10]];
-Print["2010: ", N[NewtonsDividedDifference[x, f][2010], 10]];
+Print["1930: ", NumberForm[Round[NewtonsDividedDifference[x, f][1930], 1000], DigitBlock -> 3]];
+Print["1965: ", NumberForm[Round[NewtonsDividedDifference[x, f][1965], 1000], DigitBlock -> 3]];
+Print["2010: ", NumberForm[Round[NewtonsDividedDifference[x, f][2010], 1000], DigitBlock -> 3]];
