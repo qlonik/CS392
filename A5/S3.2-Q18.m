1d2e38c82d55f788317604861c776833a4dc3f21
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-02-21 16:05:40 -0400

    refactor(S3.2-Q18): update printing lines because they didn't fit

--- A5/S3.2-Q18.m
+++ A5/S3.2-Q18.m
@@ -4,6 +4,15 @@ Import[FileNameJoin[{
 x = {1940, 1950, 1960, 1970, 1980, 1990};
 f = {132165000, 151326000, 179323000, 203302000, 226542000, 249633000};
 
-Print["1930: ", NumberForm[Round[NewtonsDividedDifference[x, f][1930], 1000], DigitBlock -> 3]];
-Print["1965: ", NumberForm[Round[NewtonsDividedDifference[x, f][1965], 1000], DigitBlock -> 3]];
-Print["2010: ", NumberForm[Round[NewtonsDividedDifference[x, f][2010], 1000], DigitBlock -> 3]];
+Print["1930: ", NumberForm[
+  Round[NewtonsDividedDifference[x, f][1930], 1000],
+  DigitBlock -> 3
+]];
+Print["1965: ", NumberForm[
+  Round[NewtonsDividedDifference[x, f][1965], 1000],
+  DigitBlock -> 3
+]];
+Print["2010: ", NumberForm[
+  Round[NewtonsDividedDifference[x, f][2010], 1000],
+  DigitBlock -> 3
+]];
