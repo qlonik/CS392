6b4ce4ec22c759dabab66ea2bf58bd8f2d537454
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-02-21 16:02:39 -0400

    fix(A5 Q4): correct the input values

--- A5/S3.2-Q18.m
+++ A5/S3.2-Q18.m
@@ -2,7 +2,7 @@ Import[FileNameJoin[{
   NotebookDirectory[], "..", "NewtonsDividedDifference.m"}]];
 
 x = {1940, 1950, 1960, 1970, 1980, 1990};
-f = {132165, 151326, 179323, 203302, 226542, 249633};
+f = {132165000, 151326000, 179323000, 203302000, 226542000, 249633000};
 
 Print["1930: ", N[NewtonsDividedDifference[x, f][1930], 10]];
 Print["1965: ", N[NewtonsDividedDifference[x, f][1965], 10]];
