b13a6f11f1f4b589ba46d5f625b08199fdb67012
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-02-28 17:51:36 -0400

    fix(A6 Q1): change data and estimation years to match book

--- A6/S3.4-Q28.js
+++ A6/S3.4-Q28.js
@@ -1,9 +1,9 @@
 const NaturalCubicSpline = require('../NaturalCubicSpline');
 
-const x = [1950, 1960, 1970, 1980, 1990, 2000];
-const fx = [151326000, 179323000, 203302000, 226542000, 249633000, 281422000];
+const x = [1940, 1950, 1960, 1970, 1980, 1990];
+const fx = [132165000, 151326000, 179323000, 203302000, 226542000, 249633000];
 const estimator = NaturalCubicSpline(x, fx);
 
-console.log('f(1940) =', estimator(1940));
-console.log('f(1975) =', estimator(1975));
-console.log('f(2020) =', estimator(2020));
+console.log('f(1930) =', estimator(1930));
+console.log('f(1965) =', estimator(1965));
+console.log('f(2010) =', estimator(2010));
