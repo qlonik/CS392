7e5bcfad83701dba6f90a56471a17426f07d9c99
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-02-27 23:49:51 -0400

    refactor(ThreePointFormula): return one function to estimate value
    Eliminate Midpoint and Endpoint functions.

--- ThreePointFormula.js
+++ ThreePointFormula.js
@@ -3,46 +3,43 @@ const math = require('mathjs');
 /**
  *
  * @param fx
- * @returns {{Endpoint: Endpoint, Midpoint: Midpoint}}
+ * @returns {Function}
  * @constructor
  */
 function ThreePointFormula(fx) {
   'use strict';
 
-  return {
-    /**
-     *
-     * @param x
-     * @param h
-     * @returns {number}
-     */
-    Endpoint: function(x, h) {
-      const xh = parseFloat(math.add(
-        math.bignumber(x), math.bignumber(h)
-      ).toString());
-      const x2h = parseFloat(math.add(
-        math.bignumber(x), math.multiply(2, math.bignumber(h))
-      ).toString());
+  /**
+   *
+   * @param {Number} x
+   * @param {Number} h
+   * @returns {Number}
+   */
+  return (x, h) => {
+    h = Math.abs(h);
+    const xp2h = parseFloat(math.add(
+      math.bignumber(x), math.multiply(2, math.bignumber(h))
+    ).toString());
+    const xph = parseFloat(math.add(
+      math.bignumber(x), math.bignumber(h)
+    ).toString());
+    const xmh = parseFloat(math.subtract(
+      math.bignumber(x), math.bignumber(h)
+    ).toString());
+    const xm2h = parseFloat(math.subtract(
+      math.bignumber(x), math.multiply(2, math.bignumber(h))
+    ).toString());
 
-      return (-3 * fx[x] + 4 * fx[xh] - fx[x2h]) / (2 * h);
-    },
-    /**
-     *
-     * @param x
-     * @param h
-     * @returns {number}
-     */
-    Midpoint: function(x, h) {
-      const xph = parseFloat(math.add(
-        math.bignumber(x), math.bignumber(h)
-      ).toString());
-      const xmh = parseFloat(math.subtract(
-        math.bignumber(x), math.bignumber(h)
-      ).toString());
-
+    if (fx[xph] && fx[xmh]) {
       return (fx[xph] - fx[xmh]) / (2 * h);
-    },
-  }
+    } else if (fx[xph] && fx[xp2h]) {
+      return (-3 * fx[x] + 4 * fx[xph] - fx[xp2h]) / (2 * h);
+    } else if (fx[xmh] && fx[xm2h]) {
+      return (-3 * fx[x] + 4 * fx[xmh] - fx[xm2h]) / (-2 * h);
+    } else {
+      throw new Error('broken!');
+    }
+  };
 }
 
 module.exports = ThreePointFormula;
