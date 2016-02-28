4d97fa7af840e1d68f3f35c07725644fd661a421
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-02-27 23:34:06 -0400

    style(A6 Q1): prettify output of the script

--- A6/S3.4-Q28.js
+++ A6/S3.4-Q28.js
@@ -4,6 +4,6 @@ const x = [1950, 1960, 1970, 1980, 1990, 2000];
 const fx = [151326000, 179323000, 203302000, 226542000, 249633000, 281422000];
 const estimator = NaturalCubicSpline(x, fx);
 
-console.log(estimator(1940));
-console.log(estimator(1975));
-console.log(estimator(2020));
+console.log('f(1940) =', estimator(1940));
+console.log('f(1975) =', estimator(1975));
+console.log('f(2020) =', estimator(2020));
