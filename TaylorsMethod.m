a4d472b76e51a72c48e764668b02b607b88e399b
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-03-20 19:31:56 -0300

    fix(TaylorsMethod): use variable x instead of t
    Variable t is reserved for numerical value. Because of this Mathematica
    is getting confused.

--- TaylorsMethod.m
+++ TaylorsMethod.m
@@ -3,14 +3,14 @@ TaylorsMethod[n_, f_, a_, b_, Num_, alpha_] := Module[{h, t, w, i},
   h = (b - a) / Num;
   t = a;
   w = alpha;
-  fWithYFn = f[t, y[t]];
-  fCopy = f[t, y[t]];
-  T[t_, y_] = 0;
+  fWithYFn = f[x, y[x]];
+  fCopy = f[x, y[x]];
+  T[x_, y_] = 0;
   For[i = 1, i <= n, i++,
-    T[t_, y_] = T[t, y] + h^(i - 1)/Factorial[i]*fCopy;
-    fCopy = D[fCopy, t] /. {y'[t] -> fWithYFn};
+    T[x_, y_] = T[x, y] + h^(i - 1)/Factorial[i]*fCopy;
+    fCopy = D[fCopy, x] /. {y'[x] -> fWithYFn};
   ];
-  T[t_, y_] = T[t, y] /. {y[t] -> y};
+  T[x_, y_] = T[x, y] /. {y[x] -> y};
   Print["t: ", N[t, 15], ", w: ", N[w, 15]];
   For[i = 1, i <= Num, i++,
     w = w + h * T[t, w];
@@ -22,14 +22,14 @@ TaylorsMethod[n_, f_, a_, b_, Num_, alpha_, fExact_] := Module[{h, t, w, i},
   h = (b - a) / Num;
   t = a;
   w = alpha;
-  fWithYFn = f[t, y[t]];
-  fCopy = f[t, y[t]];
-  T[t_, y_] = 0;
+  fWithYFn = f[x, y[x]];
+  fCopy = f[x, y[x]];
+  T[x_, y_] = 0;
   For[i = 1, i <= n, i++,
-    T[t_, y_] = T[t, y] + h^(i - 1)/Factorial[i]*fCopy;
-    fCopy = D[fCopy, t] /. {y'[t] -> fWithYFn};
+    T[x_, y_] = T[x, y] + h^(i - 1)/Factorial[i]*fCopy;
+    fCopy = D[fCopy, x] /. {y'[x] -> fWithYFn};
   ];
-  T[t_, y_] = T[t, y] /. {y[t] -> y};
+  T[x_, y_] = T[x, y] /. {y[x] -> y};
   Print["t: ", N[t, 15], ", w: ", N[w, 15], ", y: ", N[fExact[t], 15]];
   For[i = 1, i <= Num, i++,
     w = w + h * T[t, w];
