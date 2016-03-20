55e52458c335f6132cd55bd1b47733c83698b866
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-03-20 19:30:56 -0300

    fix(TaylorsMethod): calculate derivative of f(t,y) as shown in book

--- TaylorsMethod.m
+++ TaylorsMethod.m
@@ -3,10 +3,14 @@ TaylorsMethod[n_, f_, a_, b_, Num_, alpha_] := Module[{h, t, w, i},
   h = (b - a) / Num;
   t = a;
   w = alpha;
-  T[t_, y_] = Sum[
-    h^(i - 1) * D[f[t, y[t]], {t, i - 1}] / Factorial[i],
-    {i, 1, n}
-  ] /. {y[t] -> y, y'[t] -> f[t, y]};
+  fWithYFn = f[t, y[t]];
+  fCopy = f[t, y[t]];
+  T[t_, y_] = 0;
+  For[i = 1, i <= n, i++,
+    T[t_, y_] = T[t, y] + h^(i - 1)/Factorial[i]*fCopy;
+    fCopy = D[fCopy, t] /. {y'[t] -> fWithYFn};
+  ];
+  T[t_, y_] = T[t, y] /. {y[t] -> y};
   Print["t: ", N[t, 15], ", w: ", N[w, 15]];
   For[i = 1, i <= Num, i++,
     w = w + h * T[t, w];
@@ -18,10 +22,14 @@ TaylorsMethod[n_, f_, a_, b_, Num_, alpha_, fExact_] := Module[{h, t, w, i},
   h = (b - a) / Num;
   t = a;
   w = alpha;
-  T[t_, y_] = Sum[
-    h^(i - 1) * D[f[t, y[t]], {t, i - 1}] / Factorial[i],
-    {i, 1, n}
-  ] /. {y[t] -> y, y'[t] -> f[t, y]};
+  fWithYFn = f[t, y[t]];
+  fCopy = f[t, y[t]];
+  T[t_, y_] = 0;
+  For[i = 1, i <= n, i++,
+    T[t_, y_] = T[t, y] + h^(i - 1)/Factorial[i]*fCopy;
+    fCopy = D[fCopy, t] /. {y'[t] -> fWithYFn};
+  ];
+  T[t_, y_] = T[t, y] /. {y[t] -> y};
   Print["t: ", N[t, 15], ", w: ", N[w, 15], ", y: ", N[fExact[t], 15]];
   For[i = 1, i <= Num, i++,
     w = w + h * T[t, w];
