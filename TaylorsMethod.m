906faa1bbdeeed09c28a7338846619a980957e1b
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2016-03-20 19:34:50 -0300

    refactor(TaylorsMethod): rename variables

--- TaylorsMethod.m
+++ TaylorsMethod.m
@@ -4,11 +4,11 @@ TaylorsMethod[n_, f_, a_, b_, Num_, alpha_] := Module[{h, t, w, i},
   t = a;
   w = alpha;
   fWithYFn = f[x, y[x]];
-  fCopy = f[x, y[x]];
+  fForDeriv = f[x, y[x]];
   T[x_, y_] = 0;
   For[i = 1, i <= n, i++,
-    T[x_, y_] = T[x, y] + h^(i - 1)/Factorial[i]*fCopy;
-    fCopy = D[fCopy, x] /. {y'[x] -> fWithYFn};
+    T[x_, y_] = T[x, y] + h^(i - 1) / Factorial[i] * fForDeriv;
+    fForDeriv = D[fForDeriv, x] /. {y'[x] -> fWithYFn};
   ];
   T[x_, y_] = T[x, y] /. {y[x] -> y};
   Print["t: ", N[t, 15], ", w: ", N[w, 15]];
@@ -23,11 +23,11 @@ TaylorsMethod[n_, f_, a_, b_, Num_, alpha_, fExact_] := Module[{h, t, w, i},
   t = a;
   w = alpha;
   fWithYFn = f[x, y[x]];
-  fCopy = f[x, y[x]];
+  fForDeriv = f[x, y[x]];
   T[x_, y_] = 0;
   For[i = 1, i <= n, i++,
-    T[x_, y_] = T[x, y] + h^(i - 1)/Factorial[i]*fCopy;
-    fCopy = D[fCopy, x] /. {y'[x] -> fWithYFn};
+    T[x_, y_] = T[x, y] + h^(i - 1) / Factorial[i] * fForDeriv;
+    fForDeriv = D[fForDeriv, x] /. {y'[x] -> fWithYFn};
   ];
   T[x_, y_] = T[x, y] /. {y[x] -> y};
   Print["t: ", N[t, 15], ", w: ", N[w, 15], ", y: ", N[fExact[t], 15]];
